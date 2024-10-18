7f67a3dde995523ecff30b772b0be42f0d6975d3
botond0401 <botond.kov0401@gmail.com>
2024-10-18 19:28:28 +0200

    BKT data preprocessing

---
+++ src/data_preprocessing.py
@@ -0,0 +1,44 @@
+import json
+from collections import defaultdict
+import pandas as pd
+
+
+def save_assistments_BKT(
+        input_file='../data/raw/skill_builder_data.csv',
+        output_file='../data/preprocessed/assistments_skill_dict.json'
+        ) -> None:
+    """
+    Save the assistments data as a Bayesian Knowledge Tracing (BKT) dictionary.
+
+    Parameters:
+    - input_file: str, path to the input CSV file containing the assistments data.
+    - output_file: str, path to the output JSON file where the skill dictionary will be saved.
+    """
+    
+    # Load the dataset
+    df_assistments = pd.read_csv(input_file, encoding='ISO-8859-1', low_memory=False)
+
+    # Prepare the data: select relevant columns and drop missing values
+    df_data = df_assistments[['order_id', 'user_id', 'correct', 'skill_id']].dropna()
+    df_data['skill_id'] = df_data['skill_id'].astype(int)
+    df_data = df_data.sort_values(by=['user_id', 'order_id'])
+
+    # Initialize a dictionary to map skill_ids to lists of users' answers
+    skill_dict = defaultdict(list)
+
+    # Group the DataFrame by skill_id and user_id
+    for skill_id, skill_group in df_data.groupby('skill_id'):
+        for _, user_group in skill_group.groupby('user_id'):
+            # Extract the list of answers for this user and append to the skill dictionary
+            answers = user_group['correct'].tolist()
+            answers_with_brackets = [[answer] for answer in answers]
+            skill_dict[skill_id].append(answers_with_brackets)
+
+    # Convert defaultdict to a regular dictionary
+    skill_dict = dict(skill_dict)
+
+    # Save the skill dictionary to a JSON file
+    with open(output_file, 'w') as json_file:
+        json.dump(skill_dict, json_file)  # Added indent for better readability
+
+    print(f"Skill dictionary saved to {output_file}.")
